17e50acda423f7a36458b998e033449013ddb5ed
Sébastien Murgey <sebastien.murgey@rte-france.com>
2021-08-27 15:03:41 +0200

    Add some tests

---
+++ src/test/java/com/farao_community/farao/gridcapa/cgm_data_bridge/ZipFileDetectorTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ */
+package com.farao_community.farao.gridcapa.cgm_data_bridge;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author Sebastien Murgey {@literal <sebastien.murgey at rte-france.com>}
+ */
+class ZipFileDetectorTest {
+    @Test
+    void isZipForZipFileWithZipLowerCaseExtensionMustBeTrue() {
+        File file = new File(getClass().getResource("/archive.zip").getFile());
+        assertTrue(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForZipFileWithZipUpperCaseExtensionMustBeTrue() {
+        File file = new File(getClass().getResource("/archive.ZIP").getFile());
+        assertTrue(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForZipFileWithCustomExtensionMustBeTrue() {
+        File file = new File(getClass().getResource("/archive.custom").getFile());
+        assertTrue(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForZipFileWithoutExtensionMustBeTrue() {
+        File file = new File(getClass().getResource("/archive").getFile());
+        assertTrue(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForNonZipFileWithCustomExtensionMustBeFalse() {
+        File file = new File(getClass().getResource("/file.custom").getFile());
+        assertFalse(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForNonZipFileWithZipLowerCaseExtensionMustBeFalse() {
+        File file = new File(getClass().getResource("/file.zip").getFile());
+        assertFalse(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForNonZipFileWithZipUpperCaseExtensionMustBeFalse() {
+        File file = new File(getClass().getResource("/file.ZIP").getFile());
+        assertFalse(ZipFileDetector.isZip(file));
+    }
+
+    @Test
+    void isZipForNonZipFileWithoutExtensionMustBeFalse() {
+        File file = new File(getClass().getResource("/file").getFile());
+        assertFalse(ZipFileDetector.isZip(file));
+    }
+}
